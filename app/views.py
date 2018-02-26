957ae2e8051306178def8fcfb86a57e3e9500e69
Larnelle Letts <larnelleletts11@outlook.com>
2018-02-26 03:25:34 +0000

    Finally

--- app/views.py
+++ app/views.py
@@ -4,11 +4,12 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
+
+'''
 import os
 from app import app, UploadFolder, Allowed_Uploads
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from werkzeug.utils import secure_filename
-'''from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class'''
 
 
 
@@ -51,7 +52,24 @@ def upload():
             return redirect(url_for('home'))
 
     return render_template('upload.html', form = uploadform)
-
+    
+    
+    
+def get_uploaded_images():
+    Images = []
+    L_files = os.listdir("./app/static/uploads")
+    for file in L_files:
+        if file.split('.')[-1] in Allowed_Uploads:
+            Images.append(file)
+        return Images
+        
+@app.route('/files')
+def files():
+    if not session.get('logged_in'):
+        abort(401)
+        
+        Load_files_list = get_upload_images()
+        return render_templates('files.html', uploaded_images = Load_files_list)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -110,3 +128,122 @@ def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
     
+    '''
+    
+import os
+from app import app
+from flask import render_template, request, redirect, url_for, flash, session, send_from_directory, abort
+from werkzeug.utils import secure_filename
+from forms import UploadForm
+rootdir = os.getcwd()
+print rootdir
+
+
+
+
+
+###
+# Routing for your application.
+###
+
+@app.route('/')
+def home():
+    """Render website's home page."""
+    return render_template('home.html')
+
+
+@app.route('/about/')
+def about():
+    """Render the website's about page."""
+    return render_template('about.html', name="Mary Jane")
+
+
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+    error = None
+    if not session.get('logged_in'):
+        abort(401)
+    form = UploadForm()
+    # Validate file upload on submit
+    if request.method == 'POST':
+        if form.validate() == False:
+            flash_errors(form)
+        else:
+            file = form.upload.data
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File Saved', 'success')
+            return redirect(url_for('home', error=error))
+    return render_template('upload.html', form = form, error=error)
+
+@app.route('/app/static/uploads')
+def send_image(filename):
+    return send_from_directory('/app/static/uploads', filename)
+
+@app.route('/files')
+def files():
+    image_names = os.listdir('./app/static/uploads')
+    return render_template("files.html", image_names=image_names)
+    
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid username or password'
+        else:
+            session['logged_in'] = True
+            
+            flash('You were logged in', 'success')
+            return redirect(url_for('upload'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out', 'success')
+    return redirect(url_for('home'))
+
+
+###
+# The functions below should be applicable to all Flask apps.
+###
+
+# Flash errors from the form if validation fails
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s - %s" % (
+                getattr(form, field).label.text,
+                error
+), 'danger')
+
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port="8080")
